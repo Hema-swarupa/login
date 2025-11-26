06600d04bf3049e2a320d1a1d29303ac1619ac56
HEMA-SWARUPA <hemaswarupagondela@gmail.com>
2025-11-26 22:28:49 +0530

    second

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ function signup(username){
         return "signup sucessfull,please login";
     }
 }
+function login(user,pass){
+    let arr=["hema","namodh","venkatesh"];
+    if(arr.includes(user)){
+        if(pass="Emp@123"){
+            return "login sucessful";
+        }
+        else{
+            return "wrong password";
+        }
+    }
+    else{
+        return "user not found";
+    }
+}
